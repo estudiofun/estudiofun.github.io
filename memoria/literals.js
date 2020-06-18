4be1a4978eb9249d595404cff54975c3d10962ec
Anthony P. Alicea <anthonypalicea@gmail.com>
2020-06-18 17:02:20 -0400

    Literals

---
+++ memoria/literals.js
@@ -0,0 +1,32 @@
+var literals = [
+    {
+        "id": "next",
+        "english": "Next",
+        "spanish": "Otro"
+    },
+    {
+        "id": "finish",
+        "english": "Stop For Now",
+        "spanish": "Termina por ahora"
+    },
+    {
+        "id": "accuracy",
+        "english": "Accuracy",
+        "spanish": "Exactitud"
+    },
+    {
+        "id": "correct",
+        "english": "Correct!",
+        "spanish": "¡Correcto!"
+    },
+    {
+        "id": "total",
+        "english": "Total Scriptures",
+        "spanish": "Cantidad de textos"
+    },
+    {
+        "id": "refresh",
+        "english": "Refresh this page to start over",
+        "spanish": "Actualice esta página para comenzar de nuevo"
+    }
+]
