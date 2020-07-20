58115aa05d35760622981a17001627691cd5886c
Anthony P. Alicea <anthonypalicea@gmail.com>
2020-07-20 17:31:20 -0400

    RRI

---
+++ rri/rri.js
@@ -0,0 +1,27 @@
+// initialize dice
+var dice1 = ['train-s.png','road-t.png','road-s.png','train-t.png','road-c.png','train-c.png'];
+var dice2 = ['road-s.png','road-t.png','train-s.png','train-t.png','road-c.png','train-c.png'];
+var dice3 = ['train-s.png','train-t.png','road-s.png','road-t.png','road-c.png','train-c.png'];
+var dice4 = ['road-train-s.png','road-train-l.png','road-train-s.png','road-train-l.png','road-train-c.png', 'road-train-c.png'];
+
+var app = new Vue({
+    el: '#game',
+    data: {
+      dice1: dice1,
+      dice2: dice2,
+      dice3: dice3,
+      dice4: dice4,
+      roll1: 'train-s.png',
+      roll2: 'train-s.png',
+      roll3: 'train-s.png',
+      roll4: 'train-s.png'
+    },
+    methods: {
+      roll: function(lang) {
+        this.roll1 = _.shuffle(dice1)[0];
+        this.roll2 = _.shuffle(dice2)[0];
+        this.roll3 = _.shuffle(dice3)[0];
+        this.roll4 = _.shuffle(dice4)[0];
+      }
+    }
+});
